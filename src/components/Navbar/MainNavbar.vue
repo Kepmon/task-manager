31c58b9b556bae7b5a895f2dd15fcebf3cef5fe4
aleksanderwalczuk <ohleg@windowslive.com>
2023-07-14 19:55:26 +0200

    chore: remove useWindowSize()

--- src/components/Navbar/MainNavbar.vue
+++ src/components/Navbar/MainNavbar.vue
@@ -30,7 +30,7 @@
           <svg
             width="10"
             height="7"
-            class="transition-transform duration-500 hidden sm:block"
+            class="transition-transform duration-500 block sm:hidden"
             :class="{ 'rotate-180': navOpen }"
           >
             <path
@@ -43,10 +43,9 @@
         </div>
       </div>
       <div class="flex items-center gap-2 ml-auto sm:gap-3 text-white">
-        <the-button
+        <button
           @click="isAddTaskDialogShown = true"
-          :regularButton="width >= 768 ? true : false"
-          class="gap-[2px] purple-class px-4 py-[2px] rounded-2xl md:p-0 md:rounded-none"
+          class="gap-[2px] purple-class px-4 py-[2px] rounded-2xl md:p-0 regular-button"
           :class="{
             'opacity-25 cursor-not-allowed': isBoardEmpty,
             'cursor-pointer': !isBoardEmpty
@@ -54,10 +53,9 @@
         >
           <span class="leading-none text-2xl md:text-base">+</span>
           <span class="hidden md:block">Add New Task</span>
-        </the-button>
-        <the-button
+        </button>
+        <button
           @click.prevent="areBoardOptionsShown = !areBoardOptionsShown"
-          :regularButton="false"
           data-ellipsis
           aria-label="click here to see more options"
           class="px-3 py-2 rounded-md focus-visible:outline outline-[3px] outline-gray-400"
@@ -69,12 +67,12 @@
               <circle cx="2.308" cy="17.692" r="2.308" />
             </g>
           </svg>
-        </the-button>
+        </button>
       </div>
-      <Teleport v-if="width < 512" to="body">
-        <user-options class="absolute bottom-8 right-8 scale-125" />
-      </Teleport>
-      <user-options v-else class="static ml-2 sm:ml-3" />
+      <user-options
+        class="fixed sm:static bottom-8 right-8 scale-125 sm:scale-100"
+        :class="{ 'sm:fixed sm:scale-125': currentBoard == null }"
+      />
     </nav>
     <transition name="dialog">
       <task-dialog
@@ -107,7 +105,6 @@
 </template>
 
 <script setup lang="ts">
-import TheButton from '../shared/TheButton.vue'
 import MoreOptions from '../shared/MoreOptions.vue'
 import TaskDialog from '../Dialogs/TaskDialog.vue'
 import ConfirmationDialog from '../Dialogs/ConfirmationDialog.vue'
@@ -117,7 +114,6 @@ import UserOptions from '../UserOptions.vue'
 import moreOptionsPopup from '../../composables/moreOptionsPopup'
 import { useBoardsNewStore } from '../../stores/boardsNew'
 import { ref, Ref, toRefs } from 'vue'
-import { useWindowSize } from '@vueuse/core'
 
 defineProps<{
   sidebar: boolean
@@ -126,8 +122,6 @@ defineProps<{
   navOpen: boolean
 }>()
 defineEmits(['toggle-boards-nav'])
-
-const { width } = useWindowSize()
 
 const areBoardOptionsShown = ref(false)
 const isAddTaskDialogShown = ref(false)
@@ -160,7 +154,12 @@ const handleMoreOptionsFn = (
 }
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
+@screen md {
+  .regular-button {
+    @apply flex items-center grow justify-center gap-2 py-[10px] px-4 rounded-3xl;
+  }
+}
 .main-nav {
   @apply flex items-center relative px-3;
   @apply xs:px-6 shadow-xs bg-white dark:bg-gray-700;
